4825566406fa6a2f705a11002214e10e36e54c01
Reini1001 <reinmarfinkler@gmail.com>
2021-12-04 11:15:46 +0100

    + Dezember2021_4

---
+++ src/Dezember2021_5.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Dezember2021_5 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("src/inputs/input5.txt"));
+        String[] input = reader.lines().toArray(String[]::new);
+    }
+
+    private static void Aufgabe1 () {
+
+    }
+
+    private static void Aufgabe2 () {
+
+    }
+}
